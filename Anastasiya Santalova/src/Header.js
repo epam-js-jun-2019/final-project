225a55f74a40bc40de6b657088acb13b9a8314e0
Anastasiya Santalova <anastazy.santa@gmail.com>
2019-08-12 19:17:48 +0300

    Everything works, but improvements needed

---
+++ Anastasiya Santalova/src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header className="header col-12 row align-items-center border-bottom border-danger">
+        <span className="logo-image col-1">
+          <Link to="/"><img src="/myIco.png" className="logo" alt="logo"/></Link>
+        </span>
+        <span className="col-11 col-md-7">
+          <h1 className="text-warning">PokeDex</h1>
+        </span>
+        <span className="col">
+          <p className="d-inline-block">
+            <Link to="/" className="text-danger">Главная</Link>
+          </p>
+        </span>
+        <span className="col">
+          <p className="d-inline-block">
+            <Link to="/caught/" className="text-danger">Пойманные</Link>
+          </p>
+        </span>  
+      </header>
+    );
+  }
+}
+
+export default Header;
