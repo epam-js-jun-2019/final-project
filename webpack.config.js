0af1df0f39d27aa30758fc59e6e9a444407c5d63
unknown <xeniamyalinkaya@gmail.com>
2019-08-15 13:38:32 +0300

    React and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin =require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/ ,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s?css/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+        ]
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+            path: path.resolve(__dirname, '/dist'),
+        }),
+    ]
+}
