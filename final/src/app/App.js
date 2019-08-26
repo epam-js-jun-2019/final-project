9b155b881cc50dfbf4132f710ba72e38024c0ef0
trynottosmile <ivan.marakhovsky@rambler.ru>
2019-08-26 18:03:13 +0300

    init

---
+++ final/src/app/App.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react'
+import Header from '../header'
+
+const App = () => {
+    return (
+        <Fragment>
+            <Header></Header>
+        </Fragment>
+    )
+}
+
+export default App
