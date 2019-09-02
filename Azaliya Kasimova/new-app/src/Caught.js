1a84b190d40c81bcadf69fb2144e1a5358e3fe36
Azaliya Kasimova <sea-blue@inbox.ru>
2019-09-02 14:43:01 +0300

    implemented getting data from server

--- Azaliya Kasimova/new-app/src/Caught.js
+++ Azaliya Kasimova/new-app/src/Caught.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './App.css';
 
-
 class Caught extends React.Component {
-  render() {
-    return <div>Пойманные покемоны</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      caughtPokemons: [],
+    };
   }
-}
 
+  componentDidMount() {
+
+    fetch('http://localhost:3001/caughtPokemons')
+      .then(resp => resp.json())
+      .then(data => {
+        let caughtPokemons = data.map((pokemon, index) => {
+          return (
+              <div className="card">
+                <div className="card-img">
+                  <img 
+                  src = {process.env.PUBLIC_URL +'/pokemons/' + pokemon.id + '.png'}
+                  alt = {pokemon.name}/>
+                  </div>
+                  <h3>{pokemon.name}</h3>
+                </div>    
+          )
+        })
+        this.setState({caughtPokemons: caughtPokemons});
+  })
+}  
+    render() {
+      return (
+        <div className = "app">
+          <div className = "list">
+          {this.state.caughtPokemons}
+          </div>
+        </div>
+      );
+      }
+  }
+  
 export default Caught;
