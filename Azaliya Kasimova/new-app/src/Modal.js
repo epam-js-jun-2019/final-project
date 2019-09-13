63adf8ca3fdfb21f1a3e66bfa2bb8314aa611db5
Azaliya Kasimova <sea-blue@inbox.ru>
2019-09-13 14:40:27 +0300

    cleared data.json

--- Azaliya Kasimova/new-app/src/Modal.js
+++ Azaliya Kasimova/new-app/src/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const Modal = ({ handleClose, show, source, name, index, status, date}) => {
+function Modal ({ handleClose, show, source, name, index, status, date}) {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
